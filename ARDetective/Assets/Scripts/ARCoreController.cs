ed070f869ed35bbe14d476bc6c5f75978dd5b275
kevin <kevin.everett@sbcglobal.net>
2018-11-04 01:18:09 -0600

    Replaced Vuforia and its associated files in the project with ARCore. Added UI (not yet fully functional) for Investigation Mode. Rearranged files (i.e. moving scenes to Scenes folder and Scripts to Scripts folder)

---
+++ ARDetective/Assets/Scripts/ARCoreController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+/// <summary>
+/// Handles ARCore session state tracking, and user interaction with AR scene.
+/// CodelabUtils is a Google-provided script that takes care of showing popups on Android 
+/// </summary>
+public class ARCoreController : MonoBehaviour {
+
+    //this method from Google's ARCore tutorial
+    void QuitOnConnectionErrors()
+    {
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            print("ARCore requires you give permission to use the camera.");
+            StartCoroutine(CodelabUtils.ToastAndExit(
+                "Camera permission is needed to run this application.", 5));
+        }
+        else if (Session.Status.IsError())
+        {
+            // This covers a variety of errors.  See reference for details
+            // https://developers.google.com/ar/reference/unity/namespace/GoogleARCore
+            StartCoroutine(CodelabUtils.ToastAndExit(
+                "ARCore encountered a problem connecting. Please restart the app.", 5));
+            print("ARCore error:"+Session.Status.ToString());
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        QuitOnConnectionErrors();
+	}
+	
+	// Update is called once per frame (code from Google's ARCore tutorial)
+	void Update () {
+        // The session status must be Tracking in order to access the Frame.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
---
+++ ARDetective/Assets/Scripts/ButtonFxns.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Generic functions for buttons in one place for ease of reuse.
+/// </summary>
+public class ButtonFxns: MonoBehaviour {
+    [Tooltip("The text object belonging to the button that toggles the menu's visibility")]
+    public Text showHideBtn; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ExitGame()
+    {
+        //need to make this a prompt to quit instead
+        Application.Quit();
+    }
+
+    private bool isHidden;
+    public void ShowOrHideMenu(GameObject menuPanel)
+    {
+        RectTransform t = menuPanel.transform as RectTransform;
+        if (!isHidden)
+        {
+            menuPanel.transform.Translate(-t.rect.width,0f,0f);
+            isHidden = true;
+            showHideBtn.text = "<i>>></i>";// >>
+        }
+        else
+        {
+            menuPanel.transform.Translate(t.rect.width, 0f, 0f);
+            isHidden = false;
+            showHideBtn.text = "<i><<</i>";// <<
+        }
+        
+    }
+}
