d61c860ecb31586fd9512de2a824f8f54fa47386
reiddb <reiddelbello@gmail.com>
2018-11-06 21:53:44 -0600

    Created QuizMode UI and random answer selection

---
+++ ARDetective/Assets/Scripts/Answer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour {
+
+    public string AnswerString { get; set; }
+    public float Rating { get; set; }   // rating based on selection
+
+}
